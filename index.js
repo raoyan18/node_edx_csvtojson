1ff5b5c3d52293eec62462bf2dfdc19baf0f63c9
Kazi Naimul Hoque <r.aoyan18@gmail.com>
2018-10-22 19:33:55 +0600

    file write

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const csvToJson = require('csvtojson');
 
 
 const csv = csvToJson();
+const fs = require('fs');
 function csvToJSon(file) {
     csv.fromFile(file)
         .then(function (jsonObj) {
-            console.log(jsonObj);
+           fs.writeFile('json-data.json',JSON.stringify(jsonObj,null,2),function (error) {
+               if(error) return process.exit(1);
+               console.log('done');
+           });
         });
 }
 csvToJSon(process.argv[2]);
